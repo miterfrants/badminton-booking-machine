1e298ef2d6245662507dd3a11df0338f7b6707d2
Peter Huang <miterfrnats@gmail.com>
2020-06-15 00:04:06 +0800

    feat: 不同天用不同的場地位置

--- booking.js
+++ booking.js
@@ -3,11 +3,7 @@ const cron = require('node-cron');
 const moment = require('moment');
 const Web = require('./tools/web.js');
 const FetchMachine = require('./tools/fetch-machine.js');
-const urlRawData = fs.readFileSync('./config/urls.json');
-const secretsRawData = fs.readFileSync('./config/secrets.json');
 
-const urls = JSON.parse(urlRawData);
-const secrets = JSON.parse(secretsRawData);
 
 if (!fs.existsSync('./dist')) {
     fs.mkdirSync('./dist');
@@ -18,14 +14,27 @@ if (!fs.existsSync('./dist/log.txt')) {
 
 cron.schedule('55 59 15 * * *', async () => {
     try {
+        const urlRawData = fs.readFileSync('./config/urls.json');
+        const secretsRawData = fs.readFileSync('./config/secrets.json');
+        
+        const urls = JSON.parse(urlRawData);
+        const secrets = JSON.parse(secretsRawData);
+        const now = moment().add(14,'days');
+        const shortDateString = now.toISOString().split('T')[0].split('-').join('/');
+        
+        if(!urls[now.day().toString()]){
+            return;
+        }
+
         const sessionId =  await Web.login(secrets.id,secrets.pwd);
         fs.appendFileSync('./dist/log.txt', sessionId);
         console.log(`sessionId: ${sessionId}`);
-        const fetchMachine = new FetchMachine(urls, 500, sessionId, secrets.sendgridApiKey);
-        const shortDateString = moment().add(14,'days').toISOString().split('T')[0].split('-').join('/');
+        const fetchMachine = new FetchMachine(urls[now.day().toString()], 500, sessionId, secrets.sendgridApiKey);
+        
         fetchMachine.run({
-            date: shortDateString
-        });        
+            date: shortDateString,
+            dayIndex: now.day()
+        });
     } catch (error) {
 	    console.log(error);
         fs.appendFileSync('./dist/log.txt', JSON.stringify(error));
